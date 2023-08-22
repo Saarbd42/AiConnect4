97df8c80a0f5c21f5da6e5cf57e18eaf2fa43aa0
Saar Ben David <saarbd42@gmail.com>
2023-08-22 22:02:45 +0300

    Did a few changes. MinMax is still incomplete

--- Connect4/MinMaxPlayer.py
+++ Connect4/MinMaxPlayer.py
@@ -2,36 +2,52 @@ import numpy as np
 import WhoWon as ww
 import gameBoard as gb
 
+
 class MinMaxPlayer():
-    def __init__(self, playerNumber, howManyForwardPredictions):
+    def __init__(self, playerNumber, secondPlayerNum, maxForwardPredictions):
         self.playerNumber = playerNumber
+        self.originalPlayerNumber = playerNumber
+        self.originalSecondPlayerNumber = secondPlayerNum
+        self.forwardPredictions = 0
+        self.maxForwardPredictions = maxForwardPredictions
 
-    def makeAMove(self, board, howManyForwardPredictions, currentForwardPrediction):
+    # Check All possible options as it's own playerNum
+    # Try the Options
+    # See if there is an option for victory
+    # Switch to The Other Player
+    # Check All possible options
+    # Try the Options
+    # See if there is an option for lose
+    # Switch to It own playerNum
+
+    def tryAMove(self, board):
         legalMoves = self.checkLegalMoves(board)
-        for i in range(len(legalMoves)):  # TODO: CONTINUE
-            return  # TODO: CONTINUE
-
-    def checkAllOptions(self, currentBoard, playerNum):
-        legalMoves = self.checkLegalMoves(currentBoard)
+        scoresTable = [0, 0, 0, 0, 0, 0, 0]
         for i in range(len(legalMoves)):
-            copiedBoard = self.makeACopyOfTheBoard(currentBoard)
-            self.makeAMoveAndCheckForVictory(copiedBoard, legalMoves[i], playerNum)
+            copyOfTheBoard = self.copyBoard(board)
+            copyOfTheBoard.changeTheBoard(legalMoves[i], self.playerNumber)
+            score = self.scoreTheBoard(copyOfTheBoard)
+            self.switchPlayer()
+            return self.returnAScore(score, copyOfTheBoard)
 
-        return # TODO: CONTINUE
-
-    def makeAMoveAndCheckForVictory(self, fakeBoard, move, playerNum):
-        fakeBoard.changeTheBoard(move, playerNum)
-        if ww.checkIfPlayerWon(fakeBoard, playerNum):
-            return 1000
+    def returnAScore(self, score, copyOfTheBoard):
+        if score == 0 and self.forwardPredictions <= self.maxForwardPredictions:
+            return self.tryAMove(copyOfTheBoard)
         else:
-            return 0
+            return score
 
-    def makeACopyOfTheBoard(self, board):
-        copiedBoard = gb.Board
-        currentBoard = board.getCurrentState
-        copiedBoardCurrentState = np.copy(currentBoard)
-        copiedBoard.switchToANewBoard(copiedBoardCurrentState)
-        return copiedBoard
+    def scoreTheBoard(self, board):
+        if ww.checkIfPlayerWon(board, self.playerNumber):
+            if self.playerNumber == self.originalPlayerNumber:
+                return 1000
+            else:
+                return -1000
+        return 0
+
+    def copyBoard(self, board):
+        boardCurrentState = board.getCurrentState()
+        aCopyOfTheBoard = gb.CopiedBoard(boardCurrentState)
+        return aCopyOfTheBoard
 
     def checkLegalMoves(self, board):
         legalMoves = []
@@ -40,3 +56,9 @@ class MinMaxPlayer():
             if currentBoard[0][i] == 0:
                 legalMoves.append(i)
         return legalMoves
+
+    def switchPlayer(self):
+        if self.playerNumber == self.originalPlayerNumber:
+            self.playerNumber = self.originalSecondPlayerNumber
+        else:
+            self.playerNumber = self.originalPlayerNumber
--- Connect4/gameBoard.py
+++ Connect4/gameBoard.py
@@ -17,8 +17,36 @@ class Board():
             return True
         return False
 
-    def switchToANewBoard(self, newBoardCurrentState):
-        self.boardCurrentState = newBoardCurrentState
+    def changeTheBoard(self, playerMove, playerNumber):
+        if not self.isThereAnError(playerMove):
+            for i in range(6):
+                if self.boardCurrentState[5 - i][playerMove] == 0:
+                    self.boardCurrentState[5 - i][playerMove] = playerNumber
+                    break
+
+    def isThereAnError(self, playerMove):
+        if self.boardCurrentState[0][playerMove] != 0 or playerMove > 6 or playerMove < 0:
+            print("Error!")
+            return True
+        else:
+            return False
+
+
+class CopiedBoard():
+    def __init__(self, boardCurrentState):
+        self.boardCurrentState = np.copy(boardCurrentState)
+
+    def getCurrentState(self):
+        return self.boardCurrentState
+
+    def checkForTie(self):
+        count = 0
+        for i in range(7):
+            if self.boardCurrentState[0][i] != 0:
+                count += 1
+        if count == 7:
+            return True
+        return False
 
     def changeTheBoard(self, playerMove, playerNumber):
         if not self.isThereAnError(playerMove):
