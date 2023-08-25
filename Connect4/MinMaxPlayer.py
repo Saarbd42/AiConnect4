c97f66c3954cd92a4a19acd6f0d0584041ee3e58
Saar Ben David <saarbd42@gmail.com>
2023-08-25 22:56:52 +0300

    Im About to make a big erase in the MinMaxPlayer file

--- Connect4/MinMaxPlayer.py
+++ Connect4/MinMaxPlayer.py
@@ -62,3 +62,76 @@ class MinMaxPlayer():
             self.playerNumber = self.originalSecondPlayerNumber
         else:
             self.playerNumber = self.originalPlayerNumber
+
+
+class MinMaxAlgo():
+    def __init__(self, originalPlayerNumber, originalSecondPlayerNumber, maxForwardSight):
+        self.originalPlayerNumber = originalPlayerNumber
+        self.originalSecondPlayerNumber = originalSecondPlayerNumber
+        self.maxForwardSight = maxForwardSight
+
+    def trySevenMoves(self, board, playerNum, currentForwardSight):
+        scores = np.zeros(7)
+        for move in range(7):
+            scores[move] = self.scoreTheMove(board, move, playerNum, currentForwardSight)
+        return self.returnMinOrMax(scores, playerNum)
+
+    def scoreTheMove(self, board, move, playerNum, currentForwardSight):
+        if self.isMoveLegal(board, move):
+            copiedBoard = self.copyAndChangeCopiedBoard(board, move, playerNum)
+            self.scoreALegalMove(copiedBoard, playerNum, currentForwardSight)
+        else:
+            return -10000
+
+    def scoreALegalMove(self, copiedBoard, playerNum, currentForwardSight):
+        if ww.checkIfPlayerWon(copiedBoard, playerNum):
+            return self.scoreAWin(self.amITryingToMax(playerNum))
+        else:
+            if currentForwardSight >= self.maxForwardSight:
+                return 0
+            else:
+                return self.switchPlayerAndTrySevenMoves(copiedBoard, playerNum, currentForwardSight + 1)
+
+    def scoreAWin(self, tryToMax):
+        if tryToMax:
+            return 1000
+        else:
+            return -1000
+
+    def returnMinOrMax(self, scores, playerNum):
+        if self.amITryingToMax(playerNum):
+            return np.max(scores)
+        else:
+            return np.min(scores)
+
+    def amITryingToMax(self, playerNumber):
+        if playerNumber == self.originalPlayerNumber:
+            return True
+        else:
+            return False
+
+    def switchPlayerAndTrySevenMoves(self, copiedBoard, playerNum, updatedForwardSight):
+        newPlayerNum = self.switchPlayer(playerNum)
+        return self.trySevenMoves(copiedBoard, newPlayerNum, updatedForwardSight)
+
+    def switchPlayer(self, playerNum):
+        if playerNum == self.originalPlayerNumber:
+            return self.originalSecondPlayerNumber
+        else:
+            return self.originalPlayerNumber
+
+    def copyAndChangeCopiedBoard(self, board, move, playerNum):
+        copiedBoard = self.copyBoard(board)
+        copiedBoard.changeTheBoard(move, playerNum)
+        return copiedBoard
+
+    def copyBoard(self, board):
+        boardCurrentState = board.getCurrentState()
+        aCopyOfTheBoard = gb.CopiedBoard(boardCurrentState)
+        return aCopyOfTheBoard
+
+    def isMoveLegal(self, board, playerMove):
+        if board[0][playerMove] == 0:
+            return True
+        else:
+            return False
